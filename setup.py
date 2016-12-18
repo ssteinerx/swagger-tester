7d815655cfe15e9b1bce3854055c9da9ee8e8e73
Steve Steiner <ssteinerX@gmail.com>
2016-12-18 14:16:46 -0500

    Update supported pythons in setup.py

--- setup.py
+++ setup.py
@@ -45,6 +45,10 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6-dev',
+        'Programming Language :: Python :: pypy',
+
     ],
     test_suite='tests',
     tests_require=test_requirements
